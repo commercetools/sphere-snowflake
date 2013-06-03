5cdaaea21c1385f584f0bbf97f86b6b1ad7218fd
Laura Luiz <laura.luiz@commercetools.de>
2013-06-03 16:10:05 +0200

    Allow javascript fallback in set shipping address.

--- app/controllers/Checkouts.java
+++ app/controllers/Checkouts.java
@@ -1,18 +1,19 @@
 package controllers;
 
+import controllers.actions.Ajax;
 import controllers.actions.Authorization;
 import forms.addressForm.SetAddress;
 import forms.paymentForm.PaymentNetwork;
 import forms.paymentForm.PaymentNotification;
 import io.sphere.client.shop.model.Cart;
 import io.sphere.client.shop.model.PaymentState;
-import play.Play;
 import play.data.Form;
 import play.mvc.Result;
 import play.mvc.With;
 import sphere.ShopController;
 import utils.Payment;
 import views.html.checkouts;
+
 
 import java.util.List;
 
@@ -24,43 +25,71 @@ public class Checkouts extends ShopController {
     final static Form<SetAddress> setAddressForm = form(SetAddress.class);
     final static Form<PaymentNotification> paymentNotificationForm = form(PaymentNotification.class);
 
+
     public static Result show() {
         Cart cart = sphere().currentCart().fetch();
-        String checkoutId = sphere().currentCart().createCheckoutSnapshotId();
-        String submitUrl = Play.application().configuration().getString("optile.chargeUrl");
         Form<SetAddress> addressForm = setAddressForm.fill(new SetAddress(cart.getShippingAddress()));
-        return ok(checkouts.render(cart, checkoutId, submitUrl, addressForm));
+        return ok(checkouts.render(cart, addressForm, 1));
     }
 
-    public static Result submitShippingAddress() {
+    public static Result showShippingAddress() {
         Cart cart = sphere().currentCart().fetch();
-        String checkoutId = sphere().currentCart().createCheckoutSnapshotId();
-        String submitUrl = Play.application().configuration().getString("optile.chargeUrl");
+        Form<SetAddress> addressForm = setAddressForm.fill(new SetAddress(cart.getShippingAddress()));
+        return ok(checkouts.render(cart, addressForm, 2));
+    }
+
+    @With(Ajax.class)
+    public static Result setShippingAddress() {
+        Cart cart = sphere().currentCart().fetch();
         Form<SetAddress> form = setAddressForm.bindFromRequest();
+        // Case missing or invalid form data
         if (form.hasErrors()) {
             displayErrors("set-address", form);
-            return badRequest(checkouts.render(cart, checkoutId, submitUrl, form));
+            return badRequest(checkouts.render(cart, form, 2));
         }
+        // Case valid shipping address
         SetAddress setAddress = form.get();
-        sphere().currentCart().setShippingAddress(setAddress.getAddress());
-        cart = sphere().currentCart().setCountry(setAddress.getCountryCode());
+        if (setAddress.email != null) {
+            sphere().currentCart().setCustomerEmail(setAddress.email);
+        }
+        sphere().currentCart().setCountry(setAddress.getCountryCode());
+        cart = sphere().currentCart().setShippingAddress(setAddress.getAddress());
         setAddress.displaySuccessMessage(cart.getShippingAddress());
-        return ok(SetAddress.getJson(cart.getShippingAddress()));
+        return ok(checkouts.render(cart, form, 2));
     }
 
-    public static Result notification(String checkoutId) {
+    public static Result getPaymentMethod() {
+        // Case no payment needed
+        Cart cart = sphere().currentCart().fetch();
+        if (cart.getTotalPrice().getAmount().doubleValue() <= 0) {
+            return noContent();
+        }
+        // Case failed request
+        String cartSnapshot = sphere().currentCart().createCheckoutSnapshotId();
+        Payment payment = new Payment(cart, cartSnapshot);
+        if (!payment.doRequest(Payment.NATIVE_URL, Payment.Operation.LIST)) {
+            return internalServerError();
+        }
+        // Case success request
+        List<PaymentNetwork> paymentNetworks = payment.getApplicableNetworks();
+        String referredId = payment.getReferredId();
+        return ok(PaymentNetwork.getJson(paymentNetworks, referredId));
+    }
+
+    public static Result notification(String cartSnapshot) {
+        // Case missing or invalid data
         Form<PaymentNotification> form = paymentNotificationForm.bindFromRequest();
         if (form.hasErrors()) {
             return badRequest();
         }
-        System.err.println("OK from " + request().remoteAddress());
+        // Case success payment request
         PaymentNotification paymentNotification = form.get();
         System.err.println("Notification " + paymentNotification.transactionId);
         System.err.println(paymentNotification.entity + " - " + paymentNotification.statusCode + " - " + paymentNotification.reasonCode);
         System.err.println(paymentNotification.resultCode + ": " + paymentNotification.resultInfo);
         PaymentState state = paymentNotification.getPaymentState();
         if (state.equals(PaymentState.Paid)) {
-            sphere().currentCart().createOrder(checkoutId, state);
+            sphere().currentCart().createOrder(cartSnapshot, state);
         }
         return ok();
     }
@@ -78,21 +107,4 @@ public class Checkouts extends ShopController {
         // TODO Redirect to somewhere
         return badRequest("Failed...");
     }
-
-    public static Result listPaymentNetworks(String selected) {
-        Cart cart = sphere().currentCart().fetch();
-        if (cart.getTotalPrice().getAmount().doubleValue() <= 0) {
-            return noContent();
-        }
-        String checkoutId = sphere().currentCart().createCheckoutSnapshotId();
-        Payment payment = new Payment(cart, checkoutId);
-        String transactionId = payment.doRequest(Payment.NATIVE_URL, Payment.Operation.LIST);
-        if (!payment.isValidResponse(transactionId)) {
-            return noContent();
-        }
-        List<PaymentNetwork> paymentNetworks = payment.getApplicableNetworks();
-        String referredId = payment.getReferredId();
-        return ok(views.html.ajax.listPaymentNetworks.render(paymentNetworks, referredId, selected));
-    }
-
 }
