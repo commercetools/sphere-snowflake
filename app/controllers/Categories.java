d66d014e40b25078120e1ae63e41817c9cc8c5df
Laura Luiz <laura.luiz@commercetools.de>
2013-12-29 00:43:29 +0100

    Improve code for list products.

--- app/controllers/Categories.java
+++ app/controllers/Categories.java
@@ -17,6 +17,7 @@ import play.mvc.Result;
 import play.mvc.With;
 import sphere.ShopController;
 import sphere.SearchRequest;
+import utils.ProductFilters;
 import views.html.categories;
 import views.html.home;
 
@@ -45,44 +46,27 @@ public class Categories extends ShopController {
 
     public static Result listProducts(String categorySlug, String sort, int page) {
         Category category = null;
-        SearchRequest<Product> searchRequest;
-        if (categorySlug.isEmpty()) {
-            searchRequest = sphere().products().all();
-        } else {
+        List<Category> categories = new ArrayList<Category>();
+        SearchRequest<Product> searchRequest = sphere().products().all();
+        if (!categorySlug.isEmpty()) {
             category = sphere().categories().getBySlug(categorySlug);
-            if (category == null) {
-                return notFound("Category not found: " + categorySlug);
-            }
-            FilterExpression categoryFilter =
-                    new FilterExpressions.CategoriesOrSubcategories(Collections.singletonList(category));
-            searchRequest = sphere().products().filter(categoryFilter);
+            categories.add(category);
         }
-        searchRequest = filterBy(searchRequest);
+        searchRequest = filterBy(searchRequest, categories);
         searchRequest = sortBy(searchRequest, sort);
         searchRequest = paging(searchRequest, page);
         SearchResult<Product> searchResult = searchRequest.fetch();
         return ok(ListProducts.getJson(searchResult, category, sort));
     }
 
-    protected static SearchRequest<Product> filterBy(SearchRequest<Product> searchRequest) {
-        // Filters
-        List<Filter> filterList = new ArrayList<Filter>();
-        // By price
-        Filters.Price.DynamicRange filterPrice = new Filters.Price.DynamicRange().setQueryParam("price");
-        filterList.add(filterPrice);
-        // Build request
-        List<FilterExpression> filterExp = bindFiltersFromRequest(filterList);
-        searchRequest = searchRequest.filter(filterExp);
-
-        // Facets
-        List<Facet> facetList = new ArrayList<Facet>();
-        // By color
-        Facets.StringAttribute.Terms facetColor = new Facets.StringAttribute.Terms("variants.attributes.color").setQueryParam("color");
-        facetList.add(facetColor);
-        // Build request
-        List<FacetExpression> facetExp = bindFacetsFromRequest(facetList);
-        searchRequest = searchRequest.facet(facetExp);
-
+    protected static SearchRequest<Product> filterBy(SearchRequest<Product> searchRequest, List<Category> categories) {
+        // Filter by category
+        FilterExpression categoryFilter = new FilterExpressions.CategoriesOrSubcategories(categories);
+        searchRequest = searchRequest.filter(categoryFilter);
+        // Filters by request parameters
+        searchRequest = searchRequest.filter(bindFiltersFromRequest(ProductFilters.filters));
+        // Facets by request parameters
+        searchRequest = searchRequest.facet(bindFacetsFromRequest(ProductFilters.facets));
         return searchRequest;
     }
 
---
+++ app/utils/ProductFilters.java
@@ -0,0 +1,40 @@
+package utils;
+
+import io.sphere.client.facets.Facet;
+import io.sphere.client.facets.Facets;
+import io.sphere.client.filters.Filter;
+import io.sphere.client.filters.Filters;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductFilters {
+
+    /* FILTERS */
+
+    // Fulltext search
+    public static final Filters.Fulltext fulltextSearch =
+            new Filters.Fulltext().setQueryParam("q");
+
+    // Filter price
+    public static final Filters.Price.DynamicRange filterPrice =
+            new Filters.Price.DynamicRange().setQueryParam("price");
+
+    // A special collection for filters that does parameter name conflict checking
+    public static final List<Filter> filters = Arrays.<Filter>asList(
+            filterPrice,
+            fulltextSearch
+    );
+
+
+    /* FACETS */
+
+    // Facet color
+    public static final Facets.StringAttribute.Terms facetColor =
+            new Facets.StringAttribute.Terms("variants.attributes.color").setQueryParam("color");
+
+    // A special collection for facets that does parameter name conflict checking
+    public static final List<Facet> facets = Arrays.<Facet>asList(
+            facetColor
+    );
+}
