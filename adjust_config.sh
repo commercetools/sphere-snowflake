09a82c4590b6ad6d4196601f9500e19f9acfc8b1
Laura Luiz <laura.luiz@commercetools.de>
2014-01-14 19:38:00 +0100

    Fix it for real now.

--- adjust_config.sh
+++ adjust_config.sh
@@ -4,7 +4,6 @@ PLAY_CONF_FILE="conf/application.conf"
 sed -i "s;^paymill.apiKey *=.*;paymill.apiKey=\""${PAYMILL_API_KEY}"\";g" "${PLAY_CONF_FILE}"
 sed -i "s;^mail.auth.key *=.*;mail.auth.key=\""${MAIL_AUTH_KEY}"\";g" "${PLAY_CONF_FILE}"
 sed -i "s;^mail.auth.secret *=.*;mail.auth.secret=\""${MAIL_AUTH_SECRET}"\";g" "${PLAY_CONF_FILE}"
-sed -i "s;^application.secret *=.*;application.secret=\""${PLAY_APP_SECRET}"\";g" "${PLAY_CONF_FILE}"
 
 # we commit the changes in order to have them included in our push to heroku
 git config user.name "Travis CI"
