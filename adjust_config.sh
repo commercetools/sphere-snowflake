e587d32ee733d29d0242cd0faf571c4a04730425
Laura Luiz <laura.luiz@commercetools.de>
2014-06-11 11:49:20 +0200

    Update configuration script.

--- adjust_config.sh
+++ adjust_config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 PLAY_CONF_FILE="conf/application.conf"
-sed -i "s;^paymill.apiKey *=.*;paymill.apiKey=\""${PAYMILL_API_KEY}"\";g" "${PLAY_CONF_FILE}"
+sed -i "s;^paymill.key.private *=.*;paymill.key.private=\""${PAYMILL_KEY_PRIVATE}"\";g" "${PLAY_CONF_FILE}"
+sed -i "s;^paymill.key.public *=.*;paymill.key.public=\""${PAYMILL_KEY_PUBLIC}"\";g" "${PLAY_CONF_FILE}"
 sed -i "s;^mail.auth.key *=.*;mail.auth.key=\""${MAIL_AUTH_KEY}"\";g" "${PLAY_CONF_FILE}"
 sed -i "s;^mail.auth.secret *=.*;mail.auth.secret=\""${MAIL_AUTH_SECRET}"\";g" "${PLAY_CONF_FILE}"
 
