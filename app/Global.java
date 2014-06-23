c0cc73978f4355e82aad5398a151daa75f3fecaa
Laura Luiz <laura.luiz@commercetools.de>
2014-06-23 10:52:35 +0200

    Revert "Enable GZip compression."
    This reverts commit 41f518536aa84a1c4076002947a905272bc88c40.

--- app/Global.java
+++
@@ -1,9 +0,0 @@
-import play.GlobalSettings;
-import play.api.mvc.EssentialFilter;
-import play.filters.gzip.GzipFilter;
-
-public class Global extends GlobalSettings {
-    public <T extends EssentialFilter> Class<T>[] filters() {
-        return new Class[]{GzipFilter.class};
-    }
-}
